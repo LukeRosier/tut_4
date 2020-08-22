edc0eb38695bbb069f9d441fc95857a30e78e485
Luke <lukestephenrosier@gmail.com>
2020-08-22 17:34:02 +1000

    Updated Tests

--- src/test/java/tut_4/CalcTest.java
+++ src/test/java/tut_4/CalcTest.java
@@ -9,5 +9,10 @@ public class CalcTest {
 		int num1 = 1, num2 = 2, num3 = num1 + num2; 
 		assertEquals(Calc.Add(num1, num2), num3);
 	}
+	@Test 
+	public void testCalcSubtract() {
+		int num1 = 1, num2 = 2, num3 = num1 - num2; 
+		assertEquals(Calc.Subtract(num1, num2), num3);
+	}
 }
 
